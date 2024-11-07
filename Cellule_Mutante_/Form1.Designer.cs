ee65ee8aa2d879cc53d890cafb462119a9d7bec2
Sarah N <sarahniederits@gmail.com>
2024-11-07 16:12:41 +0100

    Update Form1.cs

--- Cellule_Mutante_/Form1.Designer.cs
+++ Cellule_Mutante_/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Cellule_Mutante_
 {
-    partial class Form1
+    partial class Cellule_Mutante
     {
         /// <summary>
         /// Variable nécessaire au concepteur.
@@ -30,12 +30,12 @@
         {
             this.SuspendLayout();
             // 
-            // Form1
+            // Cellule_Mutante
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.Name = "Cellule_Mutante";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
--- Cellule_Mutante_/Form1.cs
+++ Cellule_Mutante_/Form1.cs
@@ -4,24 +4,26 @@ using System.Windows.Forms;
 using Cellule_Mutante_.Controls;
 namespace Cellule_Mutante_
 {
-    public partial class Form1 : Form
+    public partial class Cellule_Mutante : Form
     {
         // Déclaration des attributs Panel et Button que l’on va
         // afficher dans notre fenêtre
         Panel pnl_main;
         Button btn_simulation;
-        public Form1()
+        public Cellule_Mutante()
         {
             InitializeComponent();
             // Initialisation d’un Panel en utilisant notre classe
             pnl_main = new MainPanel();
-            pnl_main.Location = new Point((Size.Width - pnl_main.Width) / 2 - 10,
-            (Size.Height - pnl_main.Height) / 2 - 40);
+            pnl_main.Location = new Point((Size.Width - pnl_main.Width) / 2 - 10,(Size.Height - pnl_main.Height) / 2 - 40);
             pnl_main.Anchor = AnchorStyles.None;
+
+
             // Initialisation d’un Button en utilisant notre classe
             btn_simulation = new SimulationButton();
             btn_simulation.Location = new Point(90, 390);
             btn_simulation.Anchor = AnchorStyles.None;
+
             // Ajout des éléments à notre fenêtre
             Controls.Add(pnl_main);
             Controls.Add(btn_simulation);
@@ -32,5 +34,14 @@ namespace Cellule_Mutante_
             Size = new Size(500, 500);
             Text = "Cellule Mutante";
         }
+        private void pnl_main_Paint(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics;
+            g.Clear(pnl_main.BackColor);
+            SolidBrush coloredBrush = new SolidBrush(Color.Black);
+            g.FillEllipse(coloredBrush, pnl_main.Width / 2, pnl_main.Width / 2, 10, 10);
+            g.Dispose();
+        }
+
     }
 }
