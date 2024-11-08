850f2aa6859df4fa65ebc12e380c5dde55e88265
Sarah N <sarahniederits@gmail.com>
2024-11-08 10:20:13 +0100

    Update UpdateColor and UpdateSize

--- Cellule_Mutante_/Controls/Cell.cs
+++ Cellule_Mutante_/Controls/Cell.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Data.SqlTypes;
 using System.Drawing;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
 namespace Cellule_Mutante_
 {
     public class Cell
@@ -19,7 +23,31 @@ namespace Cellule_Mutante_
             color = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
             foreach (var character in genetic)
             {
-
+                int Rnd = rnd.Next(100);
+                if (character == 'A' && Rnd <= 15)
+                {
+                    string lettre = "T";
+                    genetic = genetic + lettre;
+                }
+                else if (character == 'T' && Rnd <= 7)
+                {
+                    string lettre = "AA";
+                    genetic = genetic + lettre;
+                }
+                else if (character == 'C' && Rnd <= 21)
+                {
+                    string lettre = "G";
+                    genetic = genetic + lettre;
+                }
+                else if (character == 'G' && Rnd <= 4)
+                {
+                    string lettre = "CG";
+                    genetic = genetic + lettre;
+                }
+                else
+                {
+                    //Nothing Happens
+                }
             }
         }
         public override string ToString()
@@ -28,13 +56,22 @@ namespace Cellule_Mutante_
         }
         public string UpdateSize()
         {
-            size = 30;
+            int sizeT = genetic.Count(c => c == 'T');
+            size += 10 + (genetic.Length / 5) + (sizeT < size ? sizeT : size);
+            
+
             return size.ToString();
         }
         public void UpdateColor()
         {
-            color = Color.Red;
-        }
+            int nbTGT = Regex.Matches(genetic, Regex.Escape("TGT"), RegexOptions.IgnoreCase).Count;
+            int nbATT = Regex.Matches(genetic, Regex.Escape("ATT"), RegexOptions.IgnoreCase).Count;
+            int nbCTC = Regex.Matches(genetic, Regex.Escape("CTC"), RegexOptions.IgnoreCase).Count;
+            int nbACT = Regex.Matches(genetic, Regex.Escape("ACT"), RegexOptions.IgnoreCase).Count;
+            int nbGTC = Regex.Matches(genetic, Regex.Escape("GTC"), RegexOptions.IgnoreCase).Count;
+            int nbGAA = Regex.Matches(genetic, Regex.Escape("GAA"), RegexOptions.IgnoreCase).Count;
 
+
+        }
     }
 }
